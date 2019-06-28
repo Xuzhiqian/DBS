369f137b4c1d75d9b0e37a9c73150ee278b20891
xzq <xzq@xzq.com>
2019-06-29 02:05:07 +0800

    test6

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React,{ Component} from 'react';
 
 class KeyBox extends Component {
   render() {
-    this.props.keyType = this.props.keyType || "text";
+    let type = this.props.keyType || "text";
     return (
       <div>
         {this.props.keyName}:
-        <input type={this.props.keyType} />
+        <input type={type} />
       </div>
     )
   }

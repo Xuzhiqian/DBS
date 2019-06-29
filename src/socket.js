834971ca4bab1882c5dbf6b3527f30e2bdf17642
xzq <xzq@xzq.com>
2019-06-29 10:55:31 +0800

    fix

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ import Socket from "socket.io-client";
 
 function getSocket(){
 
-    const io = Socket(location.origin);
+    const io = Socket('localhost:8000');
 
     io.on("connect", () => {
         console.log("socket. 连接成功");

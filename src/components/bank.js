c8df3cea276e094150a5aef7cf3ad652a19d7be0
xzq <xzq@xzq.com>
2019-06-30 02:59:46 +0800

    bo

--- src/components/bank.js
+++ src/components/bank.js
@@ -78,7 +78,7 @@ class Bank extends Component {
             ];
             for (let d in res)
                 res[d].key = d.toString();
-            ReactDOM.render(<Table columns={col} dataSource={res} bordered={true} scroll={{ x: 1000,y:500 }}/>, document.getElementById("bank_table"));
+            ReactDOM.render(<Table columns={col} dataSource={res} scroll={{ x: 1000,y:500 }}/>, document.getElementById("bank_table"));
         });
     }
 

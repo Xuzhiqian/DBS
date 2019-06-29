c9a86c4a5b09e67f1bb9965c6b3ceddcd886f9be
xzq <xzq@xzq.com>
2019-06-29 20:48:24 +0800

    select1

--- src/components/bank.js
+++ src/components/bank.js
@@ -25,23 +25,47 @@ class Bank extends Component {
 
     handleChange(e) {
         let n = {};
+        if (!e.target.value) return;
         n[e.target.name] = e.target.value;
-        let adds = Object.assign({}, this.state.add, n);
-        this.setState({ add: adds });
+        let m = {};
+        m[e.target.belong] = n;
+        let st = Object.assign({}, this.state, m);
+        this.setState(st);
     }
 
     add() {
         let b = this.state.add;
         let sql = 'CALL addBank("' + b.bankName + '","' + b.bankCity + '",' + b.bankID +');';
         socket.emit("add", sql);
-        socket.on("add_err", (m) => {
-            alert(m);
+    }
+
+    find() {
+        let b = this.state.find;
+        let obj = ['bank'];
+        if (b.bankName !== 'undefined') {
+            obj.push('B_Name');
+            obj.push(b.bankName);
+        }
+        if (b.bankCity !== 'undefined') {
+            obj.push('B_City');
+            obj.push(b.bankCity);
+        }
+        if (b.bankID !== 'undefined') {
+            obj.push('B_ID');
+            obj.push(b.bankID);
+        }
+        socket.emit("find", JSON.stringify(obj));
+        socket.once("find_result", (res) => {
+            console.log(JSON.parse(res)); 
         });
     }
 
     render() {
         let add = info.map((k,index) => {
-            return <Form.Item><Input name={k.key} addonBefore={k.keyName} key={k.key} onChange={this.handleChange.bind(this)}/></Form.Item>
+            return <Form.Item><Input belong="add" name={k.key} addonBefore={k.keyName} key={k.key} onChange={this.handleChange.bind(this)}/></Form.Item>
+        });
+        let find = info.map((k, index) => {
+            return <Form.Item><Input belong="find" name={k.key} addonBefore={k.keyName} key={k.key} onChange={this.handleChange.bind(this)}/></Form.Item>
         });
         return (
             <div>
@@ -50,6 +74,13 @@ class Bank extends Component {
                     {add}
                     <Form.Item>
                         <Button type="primary" onClick={this.add.bind(this)}>添加</Button>
+                    </Form.Item>
+                </Form>
+                <h1>精确查询</h1>
+                <Form layout="inline">
+                    {find}
+                    <Form.Item>
+                        <Button type="primary" onClick={this.find.bind(this)}>查询</Button>
                     </Form.Item>
                 </Form>
             </div>      
